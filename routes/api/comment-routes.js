2c779b39de8a6a635880b9e4b016a63e5d3b97ae
Sandeep Vittala <applesandy33@gmail.com>
2022-03-07 18:13:55 -0500

    added replies functionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { addComment, 
-        removeComment 
+        removeComment,
+        addReply,
+        removeReply
     } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
@@ -11,6 +13,11 @@ router
 // /api/comments/<pizzaId>/<commentId>
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment);
 
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
+    
 module.exports = router;
